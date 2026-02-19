76ad4d6da14c9e701dba88fd72ff22d62eb97fde
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2026-02-19 16:20:19 +0000

    Initial commit

---
+++ src/main/java/com/github/FortyTwoFortyTwo/AIBuild/AIBuild.java
@@ -0,0 +1,15 @@
+package com.github.FortyTwoFortyTwo.AIBuild;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AIBuild extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
